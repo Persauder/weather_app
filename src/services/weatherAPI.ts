4784aff1e9680a1c4ca04307e9908dd85ea54c7a
Persuader <minewaysu@gmail.com>
2026-01-06 15:45:33 +0100

    feat: basic types for API; feat: interaction with API data; feat: basic project content

---
+++ src/services/weatherAPI.ts
@@ -0,0 +1,9 @@
+
+
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
+const API_KEY = import.meta.env.API_KEY;
+const UNITS = 'metric';
+
+export function getWeatherByCity(city: string) {
+
+};
---
+++ src/types/weather.ts
@@ -0,0 +1,42 @@
+type weatherData = {
+    name: string;
+    main: main;
+    weather: weather[];
+    wind: wind;
+    sys: sys;
+    coord: coord;
+    visibility: number;
+    dt: number;
+}
+
+type main = {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+}
+
+type weather = {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+}
+
+type wind = {
+    speed: number;
+    deg: number;
+    gust?: number;
+}
+
+type sys = {
+    country: string;
+    sunrise: number;
+    sunset: number;
+}
+type coord = {
+    lon: number;
+    lat: number;
+}
